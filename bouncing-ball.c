fd0ae397e36097bfa51bca82ae6d5daba0c94348
user <societyunderstander@gmail.com>
2025-03-01 08:59:59 +0200

    more ugly and aesthetic indentation stuff...

--- bouncing-ball.c
+++ bouncing-ball.c
@@ -2,56 +2,63 @@
 #include <raylib.h>
 
 
-int main(){
+int main()
+{
+
     int centerY = 100;
     int centerX = 100;
-
+    
     float velocity = 0.1;
     float acceleration = 0.3;
     float speedloss = 0.8;
-
+    
     bool falling = 1;
     bool stop = 0;
-
+    
     InitWindow(500, 500, "bouncing");
     SetTargetFPS(60);
+    
+    while(!WindowShouldClose())
+    {
 
-    while(!WindowShouldClose()){
-        
         BeginDrawing();
         ClearBackground(RAYWHITE);
         DrawCircle(centerY, centerX, 20, BLACK);
         
-        if(falling){
+        if(falling)
+        {
+
+            centerX += velocity;
+            velocity += acceleration;
         
-        centerX += velocity;
-        velocity += acceleration;
-        
-        
-            if(centerX > 480){
-                
+            if(centerX > 480)
+            {
+
                 velocity *= speedloss;
                 falling = 0;
-
             }
-            else if(speedloss < 0){
+            else if(speedloss < 0)
+            {
+
                 centerX = 480;
                 stop = 1;
             }
         }
-    else if(!falling && !stop){
-        
-        centerX -= velocity;
-        velocity -= acceleration;
-        
-        if(velocity <= 0){
+        else if(!falling && !stop)
+        {
+
+            centerX -= velocity;
+            velocity -= acceleration;
             
-            velocity = 0.1;
-            falling = 1;
-            speedloss -= 0.15;
+            if(velocity <= 0)
+            {
+                
+                velocity = 0.1;
+                falling = 1;
+                speedloss -= 0.15;
+            }
         }
-    }
-    EndDrawing();
+        EndDrawing();
     }
     
     return 0;
