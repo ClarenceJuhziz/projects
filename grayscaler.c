b792cf6c1a5a3ddaf1ba2bbae1fc8b9b96921fde
user <societyunderstander@gmail.com>
2025-03-07 00:23:37 +0200

    added some optimizations suggested by discord friends

--- grayscaler.c
+++ grayscaler.c
@@ -1,15 +1,13 @@
 #include <stdio.h>
-#include <math.h>
 
 int main()
 {
-    FILE *pic = fopen("myson.bmp", "rb");
+    FILE *pic = fopen("a.bmp", "rb");
 
     int exponent = 0;
-    int width = 0;
-    int height = 0;
-    int color = 0;
+    int width, height;
     float sum = 0;
+    int startpos = 138;
 
     fseek(pic, 0, SEEK_END);
     unsigned char buff[ftell(pic)]; // get image size THEN set a buffer with said size
@@ -17,30 +15,35 @@ int main()
 
     fread(buff, sizeof(buff), 1, pic);
 
-
     for(int i = 18; i < 22; i++) // function that gets width of the image
     {
-        width += buff[i]*pow(16, exponent);
-        exponent += 2;
+        width += buff[i] << exponent;
+        exponent++;
     }
 
     exponent= 0;
 
     for(int i = 22; i < 26; i++) // function that gets height of the image
     {
-        height += buff[i]*pow(16, exponent);
-        exponent += 2;
+        height += buff[i] << exponent;
+        exponent++;
     }
 
-    for(int i = 0; i < 3 * (width * height); i++)
-    {
-        fseek(pic, 138 + i, 0);
 
+    for(int i = 0; i < (width * height); i++)
+    {
+        fseek(pic, startpos + i, 0);
+        if((i - 1) % width == 0)
+        {
+            continue;
+        }
         sum = buff[ftell(pic)] + buff[ftell(pic) + 1] + buff[ftell(pic) + 2]; // sum of all rgb values
 
         buff[ftell(pic)] = sum / 3;
         buff[ftell(pic) + 1] = sum / 3;
         buff[ftell(pic) + 2 ] = sum / 3;
+
+        startpos += 3;
     }
 
     FILE *picout = fopen("output.bmp", "w");
